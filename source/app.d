b1e158dfe0899fa91c99b874f3ee62ba0c437616
Timo Wiren <timo.wiren@gmail.com>
2018-03-18 15:47:25 +0200

    Put image handling into its own file

--- source/app.d
+++ source/app.d
@@ -1,103 +1,12 @@
 import std.stdio;
 import std.math;
 import std.random: uniform;
+import image;
 
-struct Texture
-{
-    byte[] pixels;
-    int width;
-    int height;
-}
-
-void readTGA( string path, out Texture texture )
-{
-    try
-    {
-        auto f = File( path, "r" );
-            
-        byte[ 1 ] idLength;
-        f.rawRead( idLength );
-
-        byte[ 1 ] colorMapType;
-        f.rawRead( colorMapType );
-
-        byte[ 1 ] imageType;
-        f.rawRead( imageType );
-
-        if (imageType[ 0 ] != 2)
-        {
-            throw new Exception( "wrong TGA type: must be uncompressed true-color" );
-        }
-
-        byte[ 5 ] colorSpec;
-        f.rawRead( colorSpec );
-
-        byte[ 4 ] specBegin;
-        short[ 2 ] specDim;
-        f.rawRead( specBegin );
-        f.rawRead( specDim );
-        texture.width = specDim[ 0 ];
-        texture.height = specDim[ 1 ];
-
-        byte[ 2 ] specEnd;
-        f.rawRead( specEnd );
-
-        if (idLength[ 0 ] > 0)
-        {
-            byte[] imageId = new byte[ idLength[ 0 ] ];
-            f.rawRead( imageId );
-        }
-
-        texture.pixels = new byte[ texture.width * texture.height * 4 ];
-        f.rawRead( texture.pixels );
-    }
-    catch (Exception e)
-    {
-        writeln( "could not open ", path, ":", e );
-    } 
-}
-
-align( 1 ) struct BMPHeader
-{
-    align(1):
-    ushort magic;
-    uint fileSize;
-    ushort reserved1;
-    ushort reserved2;
-    uint bitmapOffset;
-    uint size;
-    int width;
-    int height;
-    ushort planes;
-    ushort bpp;
-    uint compression;
-    uint sizeOfBitmap;
-    int horizRes;
-    int vertRes;
-    uint colors;
-    uint importantColors;
-}
-
-void writeBMP( uint[] imageData, int width, int height )
-{
-    BMPHeader header;
-    header.magic = 0x4D42;
-    header.fileSize = cast(uint)( BMPHeader.sizeof + imageData.length * 4 );    
-    header.bitmapOffset = BMPHeader.sizeof;
-    header.size = cast(uint)BMPHeader.sizeof - 14;
-    header.width = width;
-    header.height = -height;
-    header.planes = 1;
-    header.bpp = 32;
-    header.compression = 0;
-    header.sizeOfBitmap = cast(uint)imageData.length * 4;
-    header.horizRes = 0;
-    header.vertRes = 0;
-
-    File f = File( "image.bmp", "wb" );
-    f.rawWrite( (&header)[ 0 .. 1 ] );
-    f.rawWrite( imageData );
-}
+// TODO:
+// Refraction
+// Transparency
+// Mipmapping
 
 Vec3 normalize( Vec3 v )
 {
@@ -251,7 +160,7 @@ Vec3 pathTraceRay( Vec3 rayOrigin, Vec3 rayDirection, Plane[] planes, Sphere[] s
             hitNormal = planes[ planeIndex ].normal;
             immutable float sines = sin( 10 * hitPoint.x ) * sin( 10 * hitPoint.y ) * sin( 10 * hitPoint.z );
             //hitColor = sines < 0 ? planes[ planeIndex ].color : Vec3( 1, 0, 0 );
-            hitColor = sines < 0 ? Vec3( 1, 1, 1 ) : Vec3( 1, 0, 0 );
+            hitColor = sines < 0 ? Vec3( 1, 1, 1 ) : planes[ planeIndex ].color;
             hitSmoothness = planes[ planeIndex ].smoothness;
             hitEmission = planes[ planeIndex ].emission;
         }
@@ -338,7 +247,7 @@ Vec3 pathTraceRay( Vec3 rayOrigin, Vec3 rayDirection, Plane[] planes, Sphere[] s
         immutable Vec3 jitteredReflectionDir = normalize( randomRayInHemisphere( reflectionDir ) );
         immutable Vec3 finalReflectionDir = lerp( jitteredReflectionDir, reflectionDir, hitSmoothness );
         immutable Vec3 reflectedColor = pathTraceRay( hitPoint, finalReflectionDir, planes, spheres, triangles, recursion - 1 );
-        hitColor = hitColor * reflectedColor * 0.6f + Vec3( hitEmission, hitEmission, hitEmission );
+        hitColor = (hitColor + Vec3( hitEmission, hitEmission, hitEmission ) ) * reflectedColor;
         return hitColor;
     }
     
@@ -426,7 +335,7 @@ void main()
 
     spheres[ 3 ].position = Vec3( 4, 5, -30 );
     spheres[ 3 ].radius = 2;
-    spheres[ 3 ].color = Vec3( 1, 1, 1 );
+    spheres[ 3 ].color = Vec3( 0.5f, 0.5f, 0.5f );
     spheres[ 3 ].smoothness = 0.2f;
     spheres[ 3 ].emission = 1;
 
@@ -478,7 +387,7 @@ void main()
 
                 immutable Vec3 rayDirection = normalize( fp - cameraPosition );
 
-                color = color + pathTraceRay( cameraPosition, rayDirection, planes, spheres, triangles, 3 ) * (1.0f / sampleCount);
+                color = color + pathTraceRay( cameraPosition, rayDirection, planes, spheres, triangles, 2 ) * (1.0f / sampleCount);
     
                 if (color.x > 1)
                 {
---
+++ source/image.d
@@ -0,0 +1,98 @@
+import std.stdio;
+
+struct Texture
+{
+    byte[] pixels;
+    int width;
+    int height;
+}
+
+void readTGA( string path, out Texture texture )
+{
+    try
+    {
+        auto f = File( path, "r" );
+            
+        byte[ 1 ] idLength;
+        f.rawRead( idLength );
+
+        byte[ 1 ] colorMapType;
+        f.rawRead( colorMapType );
+
+        byte[ 1 ] imageType;
+        f.rawRead( imageType );
+
+        if (imageType[ 0 ] != 2)
+        {
+            throw new Exception( "wrong TGA type: must be uncompressed true-color" );
+        }
+
+        byte[ 5 ] colorSpec;
+        f.rawRead( colorSpec );
+
+        byte[ 4 ] specBegin;
+        short[ 2 ] specDim;
+        f.rawRead( specBegin );
+        f.rawRead( specDim );
+        texture.width = specDim[ 0 ];
+        texture.height = specDim[ 1 ];
+
+        byte[ 2 ] specEnd;
+        f.rawRead( specEnd );
+
+        if (idLength[ 0 ] > 0)
+        {
+            byte[] imageId = new byte[ idLength[ 0 ] ];
+            f.rawRead( imageId );
+        }
+
+        texture.pixels = new byte[ texture.width * texture.height * 4 ];
+        f.rawRead( texture.pixels );
+    }
+    catch (Exception e)
+    {
+        writeln( "could not open ", path, ":", e );
+    } 
+}
+
+align( 1 ) struct BMPHeader
+{
+    align(1):
+    ushort magic;
+    uint fileSize;
+    ushort reserved1;
+    ushort reserved2;
+    uint bitmapOffset;
+    uint size;
+    int width;
+    int height;
+    ushort planes;
+    ushort bpp;
+    uint compression;
+    uint sizeOfBitmap;
+    int horizRes;
+    int vertRes;
+    uint colors;
+    uint importantColors;
+}
+
+void writeBMP( uint[] imageData, int width, int height )
+{
+    BMPHeader header;
+    header.magic = 0x4D42;
+    header.fileSize = cast(uint)( BMPHeader.sizeof + imageData.length * 4 );    
+    header.bitmapOffset = BMPHeader.sizeof;
+    header.size = cast(uint)BMPHeader.sizeof - 14;
+    header.width = width;
+    header.height = -height;
+    header.planes = 1;
+    header.bpp = 32;
+    header.compression = 0;
+    header.sizeOfBitmap = cast(uint)imageData.length * 4;
+    header.horizRes = 0;
+    header.vertRes = 0;
+
+    File f = File( "image.bmp", "wb" );
+    f.rawWrite( (&header)[ 0 .. 1 ] );
+    f.rawWrite( imageData );
+}
